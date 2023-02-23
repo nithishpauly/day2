e57b42032bc4c05aeb85db94300c885daf39d671
nithishpauly <94737690+nithishpauly@users.noreply.github.com>
2023-02-23 15:47:23 +0530

    Add files via upload

---
+++ nameday/src/main/java/com/example/demo/NamedayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamedayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamedayApplication.class, args);
+	}
+
+}
